0a8ffb45274fc5572c58ac26c11dcea9c7f988f7
Cristiansotogarcia <cristiansotogarcia@gmail.com>
2025-06-12 18:04:56 +0200

    Huurder Dashboard changes

---
+++ src/components/HuurderDashboard/HuurderDashboard.tsx
@@ -0,0 +1,13 @@
+// HuurderDashboard component
+import React from 'react';
+
+// HuurderDashboard component
+const HuurderDashboard: React.FC = () => {
+  return (
+    <div>
+      // HuurderDashboard content goes here
+    </div>
+  );
+};
+
+export default HuurderDashboard;
