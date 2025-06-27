39f4537e24ed7e00f2d8ef362ae3f599bbd72c0a
Cristiansotogarcia <130935481+Cristiansotogarcia@users.noreply.github.com>
2025-06-27 08:14:05 +0200

    fix supabase client env var names

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './database.types'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL
-const supabaseKey = import.meta.env.SUPABASE_ANON_KEY
+// Use the same variable names as our environment variables to avoid confusion
+const SUPABASE_URL = import.meta.env.SUPABASE_URL as string
+const SUPABASE_ANON_KEY = import.meta.env.SUPABASE_ANON_KEY as string
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY)
