4896adc57f737f0bec49d07d2b546ea9f2dba871
Cristiansotogarcia <130935481+Cristiansotogarcia@users.noreply.github.com>
2025-08-02 21:01:08 +0200

    chore: remove Reffrence Docs

--- Reffrence Docs/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import FunctionalApp from './FunctionalApp.tsx';
-import './index.css';
-
-createRoot(document.getElementById("root")!).render(
-  <FunctionalApp />
-);
-
