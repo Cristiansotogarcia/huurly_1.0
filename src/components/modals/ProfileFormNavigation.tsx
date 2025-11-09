392b66c29188e3296855b4839fe628fdff81adc4
sotocrioyo <sotocrioyo@gmail.com>
2025-11-09 09:34:36 +0100

    submit button fixed

--- src/components/modals/ProfileFormNavigation.tsx
+++ src/components/modals/ProfileFormNavigation.tsx
@@ -81,7 +81,7 @@ const ProfileFormNavigation: React.FC<ProfileFormNavigationProps> = ({
             </Button>
           ) : (
             <Button
-              type="submit"
+              type={onSubmitClick ? 'button' : 'submit'}
               className="bg-green-600 hover:bg-green-700 w-full sm:w-auto"
               disabled={isSubmitting}
               onClick={() => {
